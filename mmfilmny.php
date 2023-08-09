20f0d0b5b799f6544503cefb9880e46083d845a3
MadDeblin <123905951+MadDeblin@users.noreply.github.com>
2023-08-09 15:47:33 +0200

    Update mmfilmny.php

--- mmfilmny.php
+++ mmfilmny.php
@@ -7,7 +7,7 @@
   <title>Moviemonlthy</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="film.css">
+  <link rel="stylesheet" href="filmny.css">
 </head>
 
 <body>
